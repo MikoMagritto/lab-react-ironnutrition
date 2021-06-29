8bd25779fc92e6249405f8d6ce6b57f4c17d3cc0
lotiko <l.despres@netc.fr>
2021-06-29 20:54:22 +0200

    itér 5 start

--- src/Component/FoodBox.js
+++ src/Component/FoodBox.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 class FoodBox extends React.Component {
   state = {
-    number: '1',
+    number: 1,
   };
   handelchange(e) {
-    this.setState({ number: e.target.value });
+    let newnumber = Number(e.target.value) > 0 ? e.target.value : '0';
+    console.log(newnumber);
+    this.setState({ number: newnumber });
   }
   render() {
     return (
@@ -13,7 +15,7 @@ class FoodBox extends React.Component {
         <article className="media">
           <div className="media-left">
             <figure className="image is-64x64">
-              <img src={this.props.image} alt="tof"/>
+              <img src={this.props.image} alt="tof" />
             </figure>
           </div>
           <div className="media-content">
@@ -31,12 +33,12 @@ class FoodBox extends React.Component {
                   name="number"
                   className="input"
                   type="number"
-                  defaultValue={this.state.number}
+                  value={this.state.number}
                   onChange={(e) => this.handelchange(e)}
                 />
               </div>
               <div className="control">
-                <button className="button is-info" >+</button>
+                <button className="button is-info">+</button>
               </div>
             </div>
           </div>
