e82afb4b5447d96af12df901d3a2997cd75d5f76
lotiko <l.despres@netc.fr>
2021-06-29 20:42:31 +0200

    itér 5 start

--- src/Component/FoodBox.js
+++ src/Component/FoodBox.js
@@ -1,37 +1,49 @@
 import React from 'react';
 
-function FoodBox(props){
+class FoodBox extends React.Component {
+  state = {
+    number: '1',
+  };
+  handelchange(e) {
+    this.setState({ number: e.target.value });
+  }
+  render() {
     return (
-<div className="box">
-  <article className="media">
-    <div className="media-left">
-      <figure className="image is-64x64">
-        <img src={props.image} />
-      </figure>
-    </div>
-    <div className="media-content">
-      <div className="content">
-        <p>
-          <strong>{props.name}</strong> <br />
-          <strong>{props.calories}</strong>
-        </p>
+      <div className="box">
+        <article className="media">
+          <div className="media-left">
+            <figure className="image is-64x64">
+              <img src={this.props.image} alt="tof"/>
+            </figure>
+          </div>
+          <div className="media-content">
+            <div className="content">
+              <p>
+                <strong>{this.props.name}</strong> <br />
+                <strong>{this.props.calories}</strong>
+              </p>
+            </div>
+          </div>
+          <div className="media-right">
+            <div className="field has-addons">
+              <div className="control">
+                <input
+                  name="number"
+                  className="input"
+                  type="number"
+                  defaultValue={this.state.number}
+                  onChange={(e) => this.handelchange(e)}
+                />
+              </div>
+              <div className="control">
+                <button className="button is-info" >+</button>
+              </div>
+            </div>
+          </div>
+        </article>
       </div>
-    </div>
-    <div className="media-right">
-      <div className="field has-addons">
-        <div className="control">
-          <input className="input" type="number" value="1" />
-        </div>
-        <div className="control">
-          <button className="button is-info">
-            +
-          </button>
-        </div>
-      </div>
-    </div>
-  </article>
-</div>
     );
+  }
 }
 
 export default FoodBox;
